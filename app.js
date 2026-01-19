0dba7924c094a3226064200fe4d52b1821e74780
ilariamotta <ilaria.motta93@gmail.com>
2026-01-19 14:57:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.listen(() => {
+console.log(`Il server è in ascolto sulla porta ${port}`)
+});
+
