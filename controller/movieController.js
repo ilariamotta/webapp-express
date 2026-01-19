b78da43ea75794732d0d62c7684c8c457b14f0e5
ilariamotta <ilaria.motta93@gmail.com>
2026-01-19 15:40:27 +0100

    feat: rotte index e show

--- controller/movieController.js
+++ controller/movieController.js
@@ -1,12 +1,45 @@
 import connection from "../database/db_connections.js"
 
 function index(req, res) {
-res.json("index movies")
+const query = "SELECT * FROM `movies`";
+
+connection.query(query, (err, result)=>{
+    if (err) return next(err);
+    return res.json ({
+        results: result,
+    })
+})
 }
 
 
-function show(req, res) {
-res.json("index movies")
+function show(req, res, next) {
+const id = req.params.id
+const query = "SELECT * FROM `movies` WHERE `id` = ?";
+
+connection.query(query, [id], (err, results) => {
+    if (err) return next(err);
+
+    if(results.length === 0) {
+        res.status(404);
+        return res.json({
+            error: "Not found",
+            message: "Film non trovato",
+        })
+    }
+
+    const movie = results[0];
+
+    const reviewsQuery = "SELECT * FROM `reviews` WHERE `movie_id` = ?";
+
+    connection.query(reviewsQuery, [id], (err, reviewsResult) => {
+        if (err) return next(err);
+
+        res.json({
+            ...movie,
+            reviews: reviewsResult,
+        })
+    })
+})
 }
 
 export default {index, show}
